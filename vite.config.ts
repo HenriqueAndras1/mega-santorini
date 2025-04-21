430cdbb9d6ae51027f5b94cf485884a541eb4d6a
Henrique <119366049+HenriqueAndras1@users.noreply.github.com>
2025-04-21 16:35:31 -0300

    m

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  // Plugins que o Vite vai usar durante o desenvolvimento e build
-  plugins: [
-    react(),        // Suporte a JSX/TSX com React
-    tailwindcss(),  // Suporte ao Tailwind CSS via Vite plugin
-  ],
-
-  // Configurações do servidor local (vite dev)
+  plugins: [react(), tailwindcss()],
   server: {
-    open: true, // Abre automaticamente no navegador ao rodar `npm run dev`
+    open: true,
   },
-
-  // Caminho base para produção (importante para GitHub Pages)
-  // Sem isso, os caminhos dos arquivos no HTML quebram
-  base: '/', // <-- coloque o nome do seu repositório aqui
+  base: '/', // Caminho base para produção
 })
